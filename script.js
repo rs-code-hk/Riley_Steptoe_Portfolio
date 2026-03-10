54bf0e41b94a9153e17816e7d6534c89a69a4fe1
rs-code-hk <steptoer@wsc.school.nz>
2026-03-10 15:04:17 +1300

    Update 1.0

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-
-function closeInfo() {
-    const infoBox = document.getElementById("info")
-    infoBox.style.opacity = "0%"
-    infoBox.getElementsByTagName("h3")[0].innerHTML = "";
-    infoBox.getElementsByTagName("p")[0].innerHTML = "";
-}
-
-function infoDD() {
-    const infoBox = document.getElementById("info")
-    infoBox.style.opacity = "75%"
-    infoBox.getElementsByTagName("h3")[0].innerHTML = "Dungeons and Dragons";
-    infoBox.getElementsByTagName("p")[0].innerHTML = "Dungeons and Dragons (Or D&D) is a Role Playing Game where the players create a play a character that goes on adventures created by the Dungeon Master (DM, sometimes called a Game Master or DM)";
-}
-
-function infoMarkup() {
-    const infoBox = document.getElementById("info")
-    infoBox.style.opacity = "75%"
-    infoBox.getElementsByTagName("h3")[0].innerHTML = "Markup Languages";
-    infoBox.getElementsByTagName("p")[0].innerHTML = "Markup languages are languages used to store large amounts of data that can be compiled in another coding language.";
-}
-
 function modifyInfo(opacity, h3, p) {
     const infoBox = document.getElementById("info")
     infoBox.style.opacity = opacity;
